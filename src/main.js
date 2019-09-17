b7acab518620b6cf083ba09e67bc99b37daf8daf
qiutian00 <1476102147@qq.com>
2019-09-17 14:51:27 +0800

    fix: move the bus.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Bus from './bus'
+import Bus from './lib/bus'
 if (process.env.NODE_ENV !== 'production') require('./mock')
 
 Vue.config.productionTip = false
